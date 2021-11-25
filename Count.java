a68f1639f53f7f3ef61a1e20634da53b80661bf2
MAHESHDEOKATE <94949729+MAHESHDEOKATE@users.noreply.github.com>
2021-11-25 12:39:05 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,19 @@
+
+public class Count {
+	
+public static void main(String[]arg) {
+		
+		String s="PUNE";
+		int count=0;
+		
+		for(int i =1;i<=s.length();i++) {//s.length is find the length of string int 10
+			
+			count++;//inbuild charat in java
+		}
+		
+		System.out.println("Count of String is "+count);
+		
+	}
+
+
+}
